cdd89045152aff84c5269c2410cbe5946b4ed9ed
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-07-26 10:40:24 -0700

    Visual layout

---
+++ GymBuddies/ViewCells/RoutineDetailsExerciseTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  RoutineDetailsExerciseTableViewCell.h
+//  GymBuddies
+//
+//  Created by Gael Rodriguez Gomez on 7/26/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RoutineDetailsExerciseTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GymBuddies/ViewCells/RoutineDetailsExerciseTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RoutineDetailsExerciseTableViewCell.m
+//  GymBuddies
+//
+//  Created by Gael Rodriguez Gomez on 7/26/22.
+//
+
+#import "RoutineDetailsExerciseTableViewCell.h"
+
+@implementation RoutineDetailsExerciseTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
