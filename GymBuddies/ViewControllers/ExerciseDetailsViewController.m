631884b232a3e539a81e799ca6f227d20e8de079
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-07-31 22:44:41 -0700

    Successfully playing video

--- GymBuddies/ViewControllers/ExerciseDetailsViewController.m
+++ GymBuddies/ViewControllers/ExerciseDetailsViewController.m
@@ -7,6 +7,8 @@
 
 #import "ExerciseDetailsViewController.h"
 #import "Parse/PFImageView.h"
+#import "AVFoundation/AVFoundation.h"
+#import "AVKit/AVKit.h"
 
 static NSString * const kProfilePictureKey = @"profilePicture";
 
@@ -17,6 +19,9 @@ static NSString * const kProfilePictureKey = @"profilePicture";
 @property (weak, nonatomic) IBOutlet UILabel *authorUsername;
 @property (weak, nonatomic) IBOutlet PFImageView *bodyZoneIcon;
 @property (weak, nonatomic) IBOutlet UILabel *bodyZoneTitle;
+@property (strong, nonatomic) AVPlayerLooper *player;
+@property (strong, nonatomic) AVPlayerLayer *playerLayer;
+@property (weak, nonatomic) IBOutlet UIView *videoView;
 @end
 
 @implementation ExerciseDetailsViewController
@@ -25,6 +30,7 @@ static NSString * const kProfilePictureKey = @"profilePicture";
     [super viewDidLoad];
     
     [self setExercise:self.exercise];
+    [self playVideo];
 }
 
 
@@ -48,6 +54,33 @@ static NSString * const kProfilePictureKey = @"profilePicture";
     self.bodyZoneTitle.text = self.exercise.bodyZoneTag.title;
     
 }
+
+
+#pragma mark - Video play
+
+-(void)playVideo{
+    if(self.exercise.video != nil){
+        AVQueuePlayer *queuePlayer = [[AVQueuePlayer alloc]init];
+        
+        NSURL *url = [NSURL URLWithString:self.exercise.video.url];
+        AVAsset *asset = [AVAsset assetWithURL:url];
+        AVPlayerItem *item = [AVPlayerItem playerItemWithAsset: asset];
+        self.player = [AVPlayerLooper playerLooperWithPlayer:queuePlayer templateItem:item];
+        self.playerLayer = [AVPlayerLayer playerLayerWithPlayer:queuePlayer];
+        self.playerLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+        [self.videoView.layer addSublayer: self.playerLayer];
+        [queuePlayer play];
+    }
+}
+
+
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    self.playerLayer.frame = self.videoView.frame;
+    self.videoView.layer.masksToBounds = YES;
+}
+
+
 
 
 @end
