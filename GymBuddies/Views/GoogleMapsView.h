49fbcd5331f5f0180d1deea67d4d584f96c10126
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-08-02 22:28:11 -0700

    Reworking maps UI layout

--- GymBuddies/Views/GoogleMapsView.h
+++ GymBuddies/Views/GoogleMapsView.h
@@ -14,15 +14,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GoogleMapsView : UIView <CLLocationManagerDelegate, GMSMapViewDelegate>
 @property (weak, nonatomic) IBOutlet GMSMapView *map;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *placesSegmentedControl;
 @property (weak, nonatomic) IBOutlet UIButton *searchAddressButton;
 @property (weak, nonatomic) IBOutlet UIButton *searchEnabledButton;
+@property (weak, nonatomic) IBOutlet UIButton *placeTypeSelectionMenu;
+@property (weak, nonatomic) IBOutlet UIButton *distanceSelectionMenu;
 @property (strong, nonatomic) NSString *currentSearchAddress;
+@property (nonatomic) double searchRadius;
 @property (strong, nonatomic) CLLocationManager *manager;
 @property CLLocationCoordinate2D currentLocation;
-@property NSString *currentMarkerPlaceName;
-@property (strong, nonatomic) NSMutableArray *parksDictionary;
-@property (strong, nonatomic) NSMutableArray *gymsDictionary;
+@property NSString *currentPlaceTypeSearch;
+@property (strong, nonatomic) NSArray *placesResults;
 @property (strong, nonatomic) NSMutableArray *markers;
 -(void)setContent;
 @end
--- GymBuddies/Views/GoogleMapsView.m
+++ GymBuddies/Views/GoogleMapsView.m
@@ -7,9 +7,26 @@
 
 #import "GoogleMapsView.h"
 
-static NSString * const PLACE_TYPE_PARK = @"park";
-static NSString * const PLACE_TYPE_GYM = @"gym";
+static NSString * const kPlaceTypePark = @"park";
+static NSString * const kPlaceTypeGym = @"gym";
 static float const MAP_CAMERA_ZOOM = 13.0;
+
+static NSString * const kSearchParksActionTitle = @" Parks";
+static NSString * const kSearchGymsActionTitle = @" Gyms";
+
+static double const k500MetersRadius = 500;
+static NSString * const k500MetersActionTitle = @"500m";
+static double const k1000MetersRadius = 1000;
+static NSString * const k1000MetersActionTitle = @"1km";
+static double const k2000MetersRadius = 2000;
+static NSString * const k2000MetersActionTitle = @"2km";
+static double const k3000MetersRadius = 3000;
+static NSString * const k3000MetersActionTitle = @"3km";
+static double const k4000MetersRadius = 4000;
+static NSString * const k4000MetersActionTitle = @"4km";
+static double const k5000MetersRadius = 5000;
+static NSString * const k5000MetersActionTitle = @"5km";
+
 
 @implementation GoogleMapsView
 
@@ -25,17 +42,16 @@ static float const MAP_CAMERA_ZOOM = 13.0;
     [self.map setMyLocationEnabled:YES];
     self.map.settings.myLocationButton = YES;
     self.map.delegate = self;
+    self.currentPlaceTypeSearch = kPlaceTypePark;
+    self.searchRadius = k500MetersRadius;
 
     // Setting map's camera based on current location
     self.currentLocation = self.manager.location.coordinate;
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:self.currentLocation.latitude longitude:self.currentLocation.longitude zoom:MAP_CAMERA_ZOOM];
     self.map.camera = camera;
     
-    //Setting search button
-    self.searchEnabledButton.changesSelectionAsPrimaryAction = YES;
+    [self setPlaceTypeMenu];
     
-    // Setting initial data
-    [self setPlacesArrays];
 }
 
 
@@ -45,127 +61,159 @@ static float const MAP_CAMERA_ZOOM = 13.0;
 }
 
 
+#pragma mark - Button interaction
+
+- (IBAction)didTapSearch:(id)sender {
+    [self searchPlaces];
+}
+
+
+-(void)searchPlaces{
+    [self fetchPlacesNearby:^(NSArray *results, NSError *error) {
+        NSLog(@"tap");
+        if(error == nil){
+            self.placesResults = results;
+        }
+        [self placeMarkers];
+    }];
+}
+//- (IBAction)didTapVisitPlace:(id)sender {
+//    NSURL *googleMapsURL =[self createGoogleMapsLink];
+//    NSLog(@"%@", googleMapsURL);
+//    [[UIApplication sharedApplication]openURL:googleMapsURL options:@{} completionHandler:nil];
+//}
+
+
+-(void)setPlaceTypeMenu{
+    UIAction *searchParks = [UIAction actionWithTitle:kSearchParksActionTitle image:nil identifier:nil handler:^(UIAction *action){
+        [self changePlaceTypeSearch:kSearchParksActionTitle workoutPlace:kPlaceTypePark];
+    }];
+    searchParks.state = UIMenuElementStateOn;
+    
+    UIAction *searchGyms = [UIAction actionWithTitle:kSearchGymsActionTitle image:nil identifier:nil handler:^(UIAction *action){
+        [self changePlaceTypeSearch:kSearchGymsActionTitle workoutPlace:kPlaceTypeGym];
+    }];
+    
+    UIMenu *menu = [[UIMenu alloc]menuByReplacingChildren:[NSArray arrayWithObjects:searchParks, searchGyms, nil]];
+    self.placeTypeSelectionMenu.menu = menu;
+    self.placeTypeSelectionMenu.showsMenuAsPrimaryAction = YES;
+}
+
+
+-(void)changePlaceTypeSearch:(NSString *)actionTitle workoutPlace:(NSString *)workoutPlace{
+    for(UIAction *action in self.placeTypeSelectionMenu.menu.children){
+        if(action.title == actionTitle){
+            self.currentPlaceTypeSearch = workoutPlace;
+            [self.placeTypeSelectionMenu setTitle:actionTitle forState:UIControlStateNormal];
+            action.state = UIMenuElementStateOn;
+        } else {
+            action.state = UIMenuElementStateOff;
+        }
+    }
+}
+
+
+-(void)setSearchRadiusMenu{
+    UIAction *searchRadius500 = [UIAction actionWithTitle:k500MetersActionTitle image:nil identifier:nil handler:^(UIAction *action){
+        [self changeSearchRadius:k500MetersActionTitle radius:k500MetersRadius];
+    }];
+    
+    UIAction *searchRadius1000 = [UIAction actionWithTitle:k1000MetersActionTitle image:nil identifier:nil handler:^(UIAction *action){
+        [self changeSearchRadius:k1000MetersActionTitle radius:k1000MetersRadius];
+    }];
+    
+    UIAction *searchRadius2000 = [UIAction actionWithTitle:k2000MetersActionTitle image:nil identifier:nil handler:^(UIAction *action){
+        [self changeSearchRadius:k2000MetersActionTitle radius:k2000MetersRadius];
+    }];
+    
+    UIAction *searchRadius3000 = [UIAction actionWithTitle:k3000MetersActionTitle image:nil identifier:nil handler:^(UIAction *action){
+        [self changeSearchRadius:k3000MetersActionTitle radius:k3000MetersRadius];
+    }];
+    
+    UIAction *searchRadius4000 = [UIAction actionWithTitle:k4000MetersActionTitle image:nil identifier:nil handler:^(UIAction *action){
+        [self changeSearchRadius:k4000MetersActionTitle radius:k4000MetersRadius];
+    }];
+    
+    UIAction *searchRadius5000 = [UIAction actionWithTitle:k5000MetersActionTitle image:nil identifier:nil handler:^(UIAction *action){
+        [self changeSearchRadius:k5000MetersActionTitle radius:k5000MetersRadius];
+    }];
+    
+    UIMenu *menu = [[UIMenu alloc]menuByReplacingChildren:[NSArray arrayWithObjects: searchRadius500, searchRadius1000, searchRadius2000, searchRadius3000, searchRadius4000, searchRadius5000, nil]];
+    self.distanceSelectionMenu.showsMenuAsPrimaryAction = YES;
+    self.distanceSelectionMenu.menu = menu;
+}
+
+
+-(void)changeSearchRadius:(NSString *)actionTitle radius:(double)radius{
+    for(UIAction *action in self.placeTypeSelectionMenu.menu.children){
+        if(action.title == actionTitle){
+            self.searchRadius = radius;
+            [self.distanceSelectionMenu setTitle:actionTitle forState:UIControlStateNormal];
+            action.state = UIMenuElementStateOn;
+        } else {
+            action.state = UIMenuElementStateOff;
+        }
+    }
+}
+
 #pragma mark - Map markers
 
--(void)placeMarkers:(BOOL)isGym{
+-(void)placeMarkers{
     [self.map clear];
     [self.markers removeAllObjects];
     
-    if(isGym){
-        for(GMSPlace *gym in self.gymsDictionary){
-            
-            CLLocationDegrees latitude = gym.coordinate.latitude;
-            CLLocationDegrees longitude = gym.coordinate.longitude;
-
-            CLLocationCoordinate2D position = CLLocationCoordinate2DMake(latitude, longitude);
-            GMSMarker *marker = [GMSMarker markerWithPosition:position];
-
-            marker.title = gym.name;
-            marker.snippet = gym.formattedAddress;
-            marker.map = self.map;
-            [self.markers addObject:marker];
-        }
-    } else {
-        for(GMSPlace *park in self.parksDictionary){
-            
-            CLLocationDegrees latitude = park.coordinate.latitude;
-            CLLocationDegrees longitude = park.coordinate.longitude;
-
-            CLLocationCoordinate2D position = CLLocationCoordinate2DMake(latitude, longitude);
-            GMSMarker *marker = [GMSMarker markerWithPosition:position];
-
-            marker.title = park.name;
-            marker.snippet = park.formattedAddress;
-            marker.map = self.map;
-            [self.markers addObject:marker];
-        }
+    for(NSDictionary *place in self.placesResults){
+        CLLocationDegrees latitude = [place[@"geometry"][@"location"][@"lat"] doubleValue];
+        CLLocationDegrees longitude = [place[@"geometry"][@"location"][@"lng"] doubleValue];
+        CLLocationCoordinate2D position = CLLocationCoordinate2DMake(latitude, longitude);
+        GMSMarker *marker = [GMSMarker markerWithPosition:position];
+        marker.title = place[@"name"];
+        //marker.snippet = park.formattedAddress;
+        marker.map = self.map;
+        [self.markers addObject:marker];
     }
 }
 
 
--(NSURL *)createGoogleMapsLink{
-    NSString *URLFormattedAddress = [self.currentSearchAddress stringByReplacingOccurrencesOfString:@" " withString:@"+"];
-    NSString *googleMapsURLString = [NSString stringWithFormat:@"https://www.google.com/maps/place/%@", URLFormattedAddress];
-    NSLog(@"%@",googleMapsURLString);
-    return [NSURL URLWithString:googleMapsURLString];
-}
+//-(NSURL *)createGoogleMapsLink{
+//    NSString *URLFormattedAddress = [self.currentSearchAddress stringByReplacingOccurrencesOfString:@" " withString:@"+"];
+//    NSString *googleMapsURLString = [NSString stringWithFormat:@"https://www.google.com/maps/place/%@", URLFormattedAddress];
+//    NSLog(@"%@",googleMapsURLString);
+//    return [NSURL URLWithString:googleMapsURLString];
+//}
 
 
-- (BOOL)mapView:(GMSMapView *)mapView didTapMarker:(GMSMarker *)marker{
-    self.currentSearchAddress = marker.snippet;
-    NSLog(@"%@", self.currentSearchAddress);
-    return NO;
-}
-
-
-- (IBAction)didTapSearch:(id)sender {
-    NSURL *googleMapsURL =[self createGoogleMapsLink];
-    NSLog(@"%@", googleMapsURL);
-    [[UIApplication sharedApplication]openURL:googleMapsURL options:@{} completionHandler:nil];
-}
-
-
-- (IBAction)updateMarkers:(id)sender {
-    if(self.searchEnabledButton.isSelected){
-        [self placeMarkers:self.placesSegmentedControl.selectedSegmentIndex];
-    }
-}
+//- (BOOL)mapView:(GMSMapView *)mapView didTapMarker:(GMSMarker *)marker{
+//    return NO;
+//}
 
 
 #pragma mark - Places data fetching
 
--(void)setPlacesArrays{
-    BOOL isGym = self.placesSegmentedControl.selectedSegmentIndex;
-
-    //After fetching the places near the users, the markers are placed depending on the selected index of the segmented control
-    [self fetchPlacesNearby:PLACE_TYPE_GYM completion:^(NSMutableArray * data) {
-            self.gymsDictionary = data;
-    }];
-    [self fetchPlacesNearby:PLACE_TYPE_PARK completion:^(NSMutableArray * data) {
-            self.parksDictionary = data;
-    }];
-    
-    [self placeMarkers:isGym];
-}
-
-
--(void)fetchPlacesNearby:(NSString *)placeType completion:(void(^)(NSMutableArray *))completion{
-        
+-(void)fetchPlacesNearby:(void (^)(NSArray * results, NSError * error))completion{
     // Retrieving Google API key
     NSString *path = [[NSBundle mainBundle] pathForResource:@"../Keys" ofType:@"plist"];
     NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:path];
     NSString *googleAPIKey = [dict objectForKey:@"googleAPIKey"];
+    
     //Creating request URL using the keys, location and type of place to search
-    NSString *baseURL = [NSString stringWithFormat: @"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%f%%2C%f&radius=5000&key=%@&type=%@", self.currentLocation.latitude, self.currentLocation.longitude, googleAPIKey, placeType];
+    NSString *baseURL = [NSString stringWithFormat: @"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%f%%2C%f&radius=%f&key=%@&type=%@", self.currentLocation.latitude, self.currentLocation.longitude, self.searchRadius, googleAPIKey, self.currentPlaceTypeSearch];
     
     NSURL *URLRequest = [NSURL URLWithString:baseURL];
-    
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:URLRequest
-                                                               cachePolicy:NSURLRequestUseProtocolCachePolicy
-                                                           timeoutInterval:10.0];
-
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:URLRequest cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sharedSession];
     
-    NSURLSessionDataTask *parkDataTask = [session dataTaskWithRequest:request
-                                                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                                                    if (error) {
-                                                    } else {
-                                                        NSMutableArray *placesToReturn = [[NSMutableArray alloc]init];
-                                                        NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-                                                        NSMutableArray *places = [responseDictionary valueForKey:@"results"];
-                                                        for (NSDictionary *place in places){
-                                                            dispatch_async(dispatch_get_main_queue(), ^{
-                                                                [self getPlaceDetails:place completion:^(GMSPlace *result) {
-                                                                    if(result != nil){
-                                                                        [placesToReturn addObject:result];
-                                                                    }
-                                                                }];
-                                                            });
-                                                        }
-                                                        return completion(placesToReturn);
-                                                    }
-                                                }];
-    [parkDataTask resume];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (error == nil) {
+                NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                NSArray *places = [responseDictionary valueForKey:@"results"];
+                completion(places, error);
+            }
+        });
+            
+    }];
+    [dataTask resume];
 }
 
 
@@ -183,11 +231,4 @@ static float const MAP_CAMERA_ZOOM = 13.0;
 }
 
 
-- (IBAction)enableSearch:(id)sender {
-    if(self.searchEnabledButton.isSelected){
-        [self placeMarkers:self.placesSegmentedControl.selectedSegmentIndex];
-    } else {
-        [self.map clear];
-    }
-}
 @end
