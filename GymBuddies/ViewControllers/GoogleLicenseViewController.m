c3457cc5a38408f9f31ccac59577a0ad6f85e87a
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-08-09 11:24:42 -0700

    Including Google License

--- GymBuddies/ViewControllers/GoogleLicenseViewController.m
+++ GymBuddies/ViewControllers/GoogleLicenseViewController.m
@@ -9,13 +9,24 @@
 #import "GoogleMaps/GoogleMaps.h"
 
 @interface GoogleLicenseViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *licenseLabel;
+@property (weak, nonatomic) IBOutlet UILabel *licenseLabel1;
+@property (weak, nonatomic) IBOutlet UILabel *licenseLabel2;
+@property (weak, nonatomic) IBOutlet UILabel *licenseLabel3;
+@property (weak, nonatomic) IBOutlet UILabel *licenseLabel4;
+@property (weak, nonatomic) IBOutlet UILabel *licenseLabel5;
 @end
 
 @implementation GoogleLicenseViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString *license = [GMSServices openSourceLicenseInfo];
+    NSLog(@"%lu", [[GMSServices openSourceLicenseInfo] length]);
+    self.licenseLabel1.text = [license substringWithRange:NSMakeRange(0, 50000)];
+    self.licenseLabel2.text = [license substringWithRange:NSMakeRange(50000, 50000)];
+    self.licenseLabel3.text = [license substringWithRange:NSMakeRange(100000, 50000)];
+    self.licenseLabel4.text = [license substringWithRange:NSMakeRange(150000, 50000)];
+    self.licenseLabel5.text = [license substringWithRange:NSMakeRange(200000, 55596)];
 }
 
 /*
